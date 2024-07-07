c052f8d184dcf304976095c35cbca8ace4a25f0b
Sadia Afrin <sadialubna249@gmail.com>
2024-07-07 12:35:36 +0600

    sum of a product

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //console.log("Hello world!");
 //console.log("Good Morning!");
-let a = 10;
-let b = 5;
-console.log("sum is : ", a + b );
+//let a = 10;
+//let b = 5;
+//console.log("sum is : ", a + b );
 //this is comment.
-console.log("Sadia Afrin Lubna");
+//console.log("Sadia Afrin Lubna");
+let pencilPrice = 10;
+let eraserPrice = 5;
+console.log("the total price is : ", pencilPrice + eraserPrice, "TK"); 
